00c56efb0768231bb54f5fa77fb6ceeb12ff4f3f
Brian McKenna <brian@brianmckenna.org>
2015-11-13 22:53:39 +1100

    Update Nix expression

--- default.nix
+++ default.nix
@@ -1,39 +1,12 @@
 with import <nixpkgs> { };
 
-let purescript =
-  haskell.lib.overrideCabal haskell.packages.ghc7101.purescript (drv: rec {
-    version = "0.7.0-rc.1";
-    src = fetchFromGitHub {
-      owner = "purescript";
-      repo = "purescript";
-      rev = "v${version}";
-      sha256 = "1x0nkxp01q1w4szhzc046qn774ka0szanjj19gmwzd1bw60hzi5f";
-    };
-    buildDepends = with haskell.packages.ghc7101; [
-      Glob
-      HUnit
-      aeson
-      aeson-better-errors
-      bower-json
-      boxes
-      optparse-applicative
-      parsec
-      pattern-arrows
-      safe
-      semigroups
-      split
-      utf8-string
-    ];
-  });
-in
-
 runCommand "dummy" {
   buildInputs = [
     oraclejdk8
     (sbt.override { jre = oraclejre8; })
-    (haskell.packages.ghc7101.ghcWithPackages (pkgs: [
-      pkgs.aeson
-      purescript
+    (haskell.packages.ghc7102.ghcWithPackages (p: [
+      p.aeson
+      p.purescript
     ]))
   ];
 } ""
